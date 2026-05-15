26bb2eb179844a9168469fd773c10feaf1a6254b
ivan43lyndon <ivan43lyndon4@gmail.com>
2026-05-15 19:10:00 +0100

    Update encode.py

--- encode.py
+++ encode.py
@@ -228,9 +228,9 @@ def process_video(service, file_id, fname, data, batch_str, file_num, hold_uploa
         
         if bracket_match:
             folder_url = bracket_match.group(1)
-            target_index = int(bracket_match.group(2)) - 1  
+            target_index = int(bracket_match.group(2))  
             
-            print(f"📁 Folder parameter detected. Locating file index [{target_index + 1}]...", flush=True)
+            print(f"📁 Folder parameter detected. Locating file index [{target_index}]...", flush=True)
             file_list = []
             session_cookies = ""
             
